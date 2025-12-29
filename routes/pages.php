b303438c94930425c81bd2d214f550c16da7be81
Kevin Cardoso <kevin@allesalimentos.com.br>
2025-12-29 15:23:50 -0300

    chore: resolver conflitos em routes/pages.php

--- routes/pages.php
+++ routes/pages.php
@@ -2,9 +2,10 @@
 
 use \App\Http\Response;
 use \App\Controller\Pages;
-use \App\Controller\Common\Login;
+use \App\Controller\Common;
 
 $obRouter->get('/', [
+    // 'role' => ['admin', 'atualizador'],
     function(){
         return new Response(202, Pages\Page::getPage('Ola mund'));
     }
@@ -13,13 +14,20 @@ $obRouter->get('/', [
 // ROTA DE LOGIN (EXIBIÇÃO)
 $obRouter->get('/login', [
     function($request){
-        return new Response(200, Login::getLogin($request));
+        return new Response(200, Common\Login::getLogin($request));
     }
 ]);
 
 // ROTA DE LOGIN (POST)
 $obRouter->post('/login', [
     function($request){
-        return new Response(200, Login::setLogin($request));
+        return new Response(200, Common\Login::setLogin($request));
+    }
+]);
+
+// ROTA DE SELEÇÃO DE CONTEXTO (HUB)
+$obRouter->get('/contexto', [
+    function($request){
+        return new Response(200, Common\Contexto::getContexto($request));
     }
 ]);
