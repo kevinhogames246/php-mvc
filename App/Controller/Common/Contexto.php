b0524cb90cbf72a865f744d3c5f194bf053869aa
Kevin Q. Cardoso <kevin@allesalimentos.com.br>
2026-01-09 10:06:49 -0300

    feat(contexto): adicionar menu para usuario escolher o contexto baseado na tabela de role

--- App/Controller/Common/Contexto.php
+++ App/Controller/Common/Contexto.php
@@ -5,6 +5,7 @@ namespace App\Controller\Common;
 
 use \App\Utils\View;
 use \App\Http\Request;
+use \App\DotEnv\Environment;
 use \App\Session\Common\Login as SessionLogin;
 
 class Contexto extends Page
@@ -16,27 +17,29 @@ class Contexto extends Page
      * @return void
      */
     public static function getContexto($request){
+        Environment::load(__DIR__ . '/../');
+        $url = getenv('URL'); // $request->getRouter()->getCurrentRoute();
 
-        $roles = SessionLogin::getUserRoles();
-        $item = '';
+        $roles = SessionLogin::getUserRoles() ?? '';
+        $itens = '';
         
-        $url = $request->getRouter()->getCurrentRoute();
-
-        echo '<pre>';
-        print_r($url);
-        echo '</pre>';exit;
+        // echo '<pre>';
+        // print_r($roles);
+        // echo '</pre>';exit;
 
         // Gera os botões dinamicamente
         foreach ($roles as $role) {
-            $itens .= View::render('pages/contexto/item', [
+            $itens .= View::render('common/contexto/item', [
                 'modulo'      => $role,
                 'nome_modulo' => ucfirst($role),
                 'url'         => $url
-            ]);
+                ]);
+
+            
         }
 
         // Renderiza o conteúdo final
-        $content = View::render('pages/contexto', [
+        $content = View::render('common/contexto/contexto', [
             'itens' => $itens
         ]);
 
